c407a391ab07b11769faaa96972710fb28e89641
Nicholas Turner <bindlez@gmail.com>
2023-07-20 09:27:00 -0500

    c2-Grasper-rails-g-scaf-mod-user_assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #user has one profile
+  has_one :profile, dependent: :destroy
+
+  # User has_many Tutorials, Reviews, and Comments
+  has_many :tutorials
+  has_many :reviews
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_004057) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_142257) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.string "commentable_type", null: false
+    t.integer "commentable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "image"
+    t.text "bio"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "tutorial_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tutorial_id"], name: "index_reviews_on_tutorial_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "tutorials", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "category"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tutorials_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +64,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_004057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "reviews", "tutorials"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "tutorials", "users"
 end
